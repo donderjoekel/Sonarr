36b1aa1cb9bada08841605f01351332137e9946c
Christiaan Bloemendaal <github@tnrd.net>
2024-05-14 08:54:44 +0200

    Fix migration test

--- src/NzbDrone.Core.Test/Datastore/Migration/158_cdh_per_downloadclientFixture.cs
+++ src/NzbDrone.Core.Test/Datastore/Migration/158_cdh_per_downloadclientFixture.cs
@@ -4,7 +4,6 @@ using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using NzbDrone.Common.Serializer;
 using NzbDrone.Core.Datastore.Migration;
-using NzbDrone.Core.Download.Clients.RTorrent;
 using NzbDrone.Core.Test.Framework;
 
 namespace NzbDrone.Core.Test.Datastore.Migration
@@ -71,34 +70,6 @@ namespace NzbDrone.Core.Test.Datastore.Migration
 
             items.Should().HaveCount(1);
             items.First().RemoveCompletedDownloads.Should().BeTrue();
-            items.First().RemoveFailedDownloads.Should().BeTrue();
-        }
-
-        [Test]
-        public void should_disable_remove_for_existing_rtorrent()
-        {
-            var db = WithMigrationTestDb(c =>
-            {
-                c.Insert.IntoTable("DownloadClients").Row(new
-                {
-                    Enable = true,
-                    Name = "RTorrent",
-                    Implementation = "RTorrent",
-                    Priority = 1,
-                    Settings = new RTorrentSettings
-                    {
-                        Host = "127.0.0.1",
-                        TvCategory = "abc",
-                        UrlBase = "/my/"
-                    }.ToJson(),
-                    ConfigContract = "RTorrentSettings"
-                });
-            });
-
-            var items = db.Query<DownloadClientDefinition158>("SELECT * FROM \"DownloadClients\"");
-
-            items.Should().HaveCount(1);
-            items.First().RemoveCompletedDownloads.Should().BeFalse();
             items.First().RemoveFailedDownloads.Should().BeTrue();
         }
     }
